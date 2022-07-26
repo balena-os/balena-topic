8ec6a2ebb61783db4f0d8b370856afbe575a37ae
Francois <francois.loubser@topic.nl>
2022-07-26 10:51:31 +0200

    autobuild: only build for tdpzu9
    Repo currently only supports the tdpzu9 device, so don't run coffeemaker.sh
    Change build command to indicate which machine to build using '-m' option.
    
    Changelog-entry: tdpzu9 device support
    Signed-off-by: Francois Loubser <francois.loubser@topic.nl>

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 git submodule update --init --recursive
 rm -rf build/artefacts
-./scripts/coffeemaker.sh
-./balena-yocto-scripts/build/barys --continue --development-image
+#./scripts/coffeemaker.sh
 mkdir -p build/artefacts
 for m in t*.coffee
 do
 	MACHINE=`basename $m .coffee`
+	./balena-yocto-scripts/build/barys --continue --development-image -m ${MACHINE}
 	xz -3 < build/tmp/deploy/images/${MACHINE}/balena-image-${MACHINE}.balenaos-img > build/artefacts/balena-image-${MACHINE}.balenaos-img.xz &
 done
 wait
